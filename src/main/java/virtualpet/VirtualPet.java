0461665e98fd9ee5de0b363198253c0efb8b1121
Jeff Salisbury <Salisbury.jeffery@gmail.com>
2018-01-26 13:56:31 -0500

    If and loop working

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -1,26 +1,25 @@
 package virtualpet;
 
 public class VirtualPet {
-	
-	
-	
+
+	private int tick;
+
 	private int hunger;
-	
+
 	private int thirst;
-	
+
 	private int boredom;
-	
+
 	private int tiredness;
-	
+
 	public VirtualPet(int hunger, int thirst, int boredom, int tiredness) {
-		
+
 		this.hunger = hunger;
 		this.thirst = thirst;
 		this.boredom = boredom;
 		this.tiredness = tiredness;
 	}
-	
-	
+
 	public int getHunger() {
 		return hunger;
 	}
@@ -28,15 +27,46 @@ public class VirtualPet {
 	public int getThirst() {
 		return thirst;
 	}
-	
+
 	public int getBoredom() {
 		return boredom;
 	}
-	
+
 	public int getTiredness() {
 		return tiredness;
 	}
-	
-	
-}
 
+	public int getTick() {
+		return tick;
+	}
+
+	public void feeding() {
+		int eating = 5;
+		hunger -= eating;
+		thirst += eating;
+		
+	}
+
+	public void quenching() {
+		int drink = 5;
+		thirst -= drink;
+		hunger += drink;
+	}
+
+	public void activity() {
+		int fun = 5;
+		boredom -= fun;
+		tiredness +=fun;
+	}
+
+	public void rest() {
+		int nap = 5;
+		tiredness -= nap;
+		boredom += nap;
+	}
+	
+	public void countTick() {
+		
+		
+	}
+}
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -7,28 +7,52 @@ public class VirtualPetApp {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-		
-		VirtualPet startPet = new VirtualPet(50,50,50,50);
-		
+
+		VirtualPet startPet = new VirtualPet(25, 25, 25, 25);
+
 		System.out.println("Please give your virtual pet a name: ");
 		String name = input.nextLine();
+		String move;
+		do {
+			System.out.println(name + " The Virtual Pet!");
+			System.out.println("");
+			System.out.println("Hunger: " + startPet.getHunger() + "/50");
+			System.out.println("Thirst: " + startPet.getThirst() + "/50");
+			System.out.println("Boredom: " + startPet.getBoredom() + "/50");
+			System.out.println("Tiredness: " + startPet.getTiredness() + "/50");
+			System.out.println("");
+			System.out.println("What shall you and " + name + " do today?");
+
+			System.out.println("1. Feed " + name);
+			System.out.println("2. Water " + name);
+			System.out.println("3. Play with " + name);
+			System.out.println("4. Let " + name + " nap");
+			System.out.println("5. Do nothing with " + name);
+			System.out.println("6. Quit and never see " + name + " again.");
+
+			move = input.nextLine();
+			
+			if (move.equals("1") && !(startPet.getHunger()== 0)) {
+				startPet.feeding();
+			}
+			
+			if (move.equals("2") && !(startPet.getThirst()==0)) {
+				startPet.quenching();
+			}
+			
+			if (move.equals("3") && !(startPet.getBoredom()==0)) {
+				startPet.activity();
+			}
+			
+			if (move.equals("4") && !(startPet.getTiredness()==0)) {
+				startPet.rest();
+			}
+			
+		} while (!(move.equals("6")));
+
 		
-		System.out.println(name + " The Virtual Pet!");
-		System.out.println("");
-		System.out.println("Hunger: " + startPet.getHunger()+"/50");
-		System.out.println("Thirst: " + startPet.getThirst()+"/50");
-		System.out.println("Boredom: " + startPet.getBoredom()+"/50");
-		System.out.println("Tiredness: " + startPet.getTiredness()+ "/50");
-		System.out.println("");
-		System.out.println("What shall you and " + name + " do today?");
-		
-		
-		System.out.println("1. Feed " + name);
-		System.out.println("2. Water " + name);
-		System.out.println("3. Play with "  + name);
-		System.out.println("4. Let " + name + " nap");
-		System.out.println("5. Do nothing with " + name);
-		
+		System.out.println("Thanks for playing with " + name +" and remeber all the good memories you got to share together...");
+
 		input.close();
 	}
 
